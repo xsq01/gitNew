ebec6e8929d9e23b636e993b8acde4b946810c8e
xsq <1805992019@qq.com>
2023-02-10 22:42:24 +0800

    gitNewXSQ@1.0.2

--- src/Controller.java
+++ src/Controller.java
@@ -2,6 +2,8 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("gitNew@1.0.0");
         System.out.println("gitNew@1.0.1");
+        System.out.println("hot-fix");
+
 
     }
 }
